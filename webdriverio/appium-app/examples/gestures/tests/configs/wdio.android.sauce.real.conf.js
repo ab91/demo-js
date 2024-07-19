b92d9e51828affd854ee93a52c59ac9a3fe881f2
Abe Biswas <abe.biswas@saucelabs.com>
2024-07-19 05:34:27 -0500

    feat: Update config for device diversity

--- webdriverio/appium-app/examples/gestures/tests/configs/wdio.android.sauce.real.conf.js
+++ webdriverio/appium-app/examples/gestures/tests/configs/wdio.android.sauce.real.conf.js
@@ -7,7 +7,7 @@ const {config} = require('./wdio.shared.sauce.conf');
 // http://appium.io/docs/en/writing-running-appium/caps/#general-capabilities
 config.capabilities = [
   {
-    deviceName: 'Samsung Galaxy S10',
+    deviceName: 'Samsung.*',
     platformName: 'Android',
     orientation: 'PORTRAIT',
     automationName: 'UiAutomator2',
@@ -15,17 +15,35 @@ config.capabilities = [
     // see https://wiki.saucelabs.com/display/DOCS/Application+Storage for more information
     app: 'storage:filename=sample-app-android.apk',
     appWaitActivity: 'com.swaglabsmobileapp.MainActivity',
-    // Keep the device connected between tests so we don't need to wait for the cleaning process
-    cacheId: 'jsy1v49pn10',
-    // You can also provide the Appium version
-    // we need to have at least 1.19.0 for the new Android Gesture functions
-    appiumVersion: '1.19.0',
     newCommandTimeout: 240,
-    // Always default the language to a language you prefer so you know the app language is always as expected
     language: 'en',
     locale: 'en',
-    // Add a name to the test
-    name: 'Appium Gestures',
+  },
+  {
+    deviceName: 'OnePlus.*',
+    platformName: 'Android',
+    orientation: 'PORTRAIT',
+    automationName: 'UiAutomator2',
+    // The path to the app that has been uploaded to the Sauce Storage,
+    // see https://wiki.saucelabs.com/display/DOCS/Application+Storage for more information
+    app: 'storage:filename=sample-app-android.apk',
+    appWaitActivity: 'com.swaglabsmobileapp.MainActivity',
+    newCommandTimeout: 240,
+    language: 'en',
+    locale: 'en',
+  },
+  {
+    deviceName: 'Google.*',
+    platformName: 'Android',
+    orientation: 'PORTRAIT',
+    automationName: 'UiAutomator2',
+    // The path to the app that has been uploaded to the Sauce Storage,
+    // see https://wiki.saucelabs.com/display/DOCS/Application+Storage for more information
+    app: 'storage:filename=sample-app-android.apk',
+    appWaitActivity: 'com.swaglabsmobileapp.MainActivity',
+    newCommandTimeout: 240,
+    language: 'en',
+    locale: 'en',
   },
 ];
 
